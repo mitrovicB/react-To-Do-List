8e812a7e6ade93c4fadbf465a95acfbe4ac3bb9a
Branka Mitrovic <bramit93.gmail.com>
2021-02-23 10:06:07 +0100

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.tasks.map(task => (
+          <div>
+            <li key={task.id}>{task.text}<button>-</button></li>
+          </div>
+        ))}
+     </ul>
+    );
+  }
 }
 
-export default App;
+class ToDoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      checked: true,
+      tasks: [],
+      text: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  }
+  
+  handleClick(event) {
+    event.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+
+    const newTask = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      tasks: state.tasks.concat(newTask),
+      text: ''
+    }));
+
+    console.log("clicked!");
+    console.log(newTask);
+  }
+ 
+
+  render() {
+    return (
+      <div>
+        <h1>TO DO LIST</h1>
+        <div>
+          <input id="new-todo" 
+          onChange={this.handleChange}
+          value={this.state.text}
+          />
+         <button onClick={this.handleClick}>Add task</button>
+        </div>
+        <ToDoList tasks={this.state.tasks} />
+      </div>
+    );
+  }
+}
+
+
+export default ToDoApp;
